ddc564c5b2759051574285ef346b0126f0b8535a
Tristan Bilot <tristan_bilot@carrefour.com>
2021-03-26 15:37:58 +0100

    feat(ai): start ai stuff

--- kobo_ai.py
+++ kobo_ai.py
@@ -3,8 +3,10 @@ from enum import Enum
 from dataclasses import dataclass
 from typing import List
 import random
+import operator
 
 class Rank(Enum):
+    TEN = 0
     ACE = 1
     TWO = 2
     THREE = 3
@@ -14,10 +16,9 @@ class Rank(Enum):
     SEVEN = 7 
     EIGHT = 8
     NINE = 9
-    TEN = 10
-    JACK = 11
-    QUEEN = 12
-    KING = 13
+    JACK = 10
+    QUEEN = 11
+    KING = 12
 
     def format(self):
         traductions = [str(i+1) for i in range(10)] + ['J', 'Q', 'K']
@@ -62,6 +63,89 @@ class Card:
     def __hash__(self):
         return hash((self.rank, self.suit))
 
+class CommandKeys(Enum):
+    QuitKey = 'Q'
+    KoboKey = 'K'
+
+class PlayerInput:
+    def __init__(self):
+        self.is_index = False
+        self.is_command = False
+        self.is_kobo = False
+        self.is_quit_key = False
+        self.value = ''
+
+    def input_loop(self, nb_cards, msg='', accept_indexes=False, accept_commands=False) -> int:
+        """ loop asking the user to enter an answer.
+        """
+        self.value = self._input(msg)
+        
+        while (accept_indexes and accept_commands and not self._is_correct(nb_cards)) \
+            or (accept_indexes and not accept_commands and not self._check_card_index(nb_cards)) \
+            or (accept_commands and not accept_indexes and not self._check_command()):
+            print(self._invalid_input_msg(nb_cards))
+            print(accept_commands and not accept_indexes and not self._check_command())
+            self.value = self._input(msg)
+        if self._check_kobo():
+            self.is_kobo = True
+        if self._check_card_index(nb_cards):
+            self.value = int(self.value) - 1
+            self.is_index = True
+        elif self._check_command():
+            self.is_command = True
+        if self.is_command and self.value == CommandKeys.QuitKey:
+            self.is_quit_key = True
+
+        # print(self.is_index)
+        # print(self.is_command)
+        # print(self.is_kobo)
+        # print(self.is_quit_key)
+
+    def _input(self, msg: str='') -> str:
+        return input(msg)
+
+    def _is_correct(self, nb_cards: int) -> bool:
+        """ returns wether the user's input is correct. 
+        In bound of the number of cards or a command.
+        """
+        return self._check_card_index(nb_cards) or self._check_command()
+
+    def _check_card_index(self, nb_cards: int) -> bool:
+        inp = self.value.split()
+        if not 1 >= len(inp) <= 2:
+            return False
+        inp = inp[0]
+        if not inp.isdigit():
+            return False
+        inp = int(inp)
+        return 1 <= inp <= nb_cards
+
+    def _check_command(self) -> bool:
+        inp = self.value.split()
+        print(inp)
+        if not 1 >= len(inp) <= 2:
+            return False
+        cmd = inp[0]
+        return self._is_valid_command(cmd)
+
+    def _check_kobo(self) -> bool:
+        inp = self.value.split()
+        if len(inp) != 2:
+            return False
+        cmd = inp[1]
+        is_kobo = self._is_valid_command(cmd) and cmd == CommandKeys.KoboKey
+        if is_kobo:
+            self.value = cmd[0]
+        return is_kobo
+
+    def _is_valid_command(self, cmd: str):
+        return isinstance(cmd, str) and cmd in set(item.value for item in CommandKeys)
+
+    def _invalid_input_msg(self, nb_cards: int):
+        digit_valid = [*range(1, nb_cards + 1)]
+        return 'Invalid command, valid commands: {}'.format( \
+            [*digit_valid, *list(map(lambda x: x.value, [*CommandKeys]))])
+
 class Game:
     def __init__(self, nb_cards=4):
         self.nb_cards = nb_cards
@@ -69,7 +153,6 @@ class Game:
         self._init_game()
         self._displayed_cards = [0, 1]
         self._should_display_cards = True
-        Game.QuitKey = 'Q'
 
     def pop_card(self):
         if len(self.deck) == 0:
@@ -141,11 +224,16 @@ class PlayerI:
     def __init__(self, game):
         self.game = game
         self.victories = 0
+        self.is_kobo = False
 
     def set_cards(self, cards):
         self.cards = cards
         for c in self.cards[:2]:
             c.discover(True)
+
+    @property
+    def nb_cards(self):
+        return len(self.cards)
 
     def win(self):
         self.victories += 1
@@ -156,12 +244,11 @@ class PlayerI:
     def _apply_card_effects(self, thrown_cards):
         pass
 
-    def _substitute_card(self, answer: int, deck_card: Card):
-        old_cards = self.cards.copy()
-        selected = self.cards[answer]
+    def _substitute_card(self, index: int, deck_card: Card):
+        selected = self.cards[index]
         deck_card.discover(True)
-        thrown_cards = [self.cards.pop(answer)]
-        self.cards.insert(answer, deck_card)
+        thrown_cards = [self.cards.pop(index)]
+        self.cards.insert(index, deck_card)
 
         # remove all duplicates of the chosen card
         updated_cards = self.cards.copy()
@@ -170,14 +257,16 @@ class PlayerI:
             if c.is_discovered() and c == selected:
                 thrown_cards.append(updated_cards.pop(i))
         self.cards = updated_cards
-
-        print(old_cards)
-        print(updated_cards)
-        print(len(thrown_cards))
         return thrown_cards
 
     def _do_not_substitute_card(self, deck_card: Card):
-        thrown_cards = [card for card in self.cards if card == deck_card] + [deck_card]
+        thrown_cards = []
+        updated_cards = self.cards.copy()
+        for i in range(len(self.cards)):
+            c = self.cards[i]
+            if c.is_discovered() and c == deck_card:
+                thrown_cards.append(updated_cards.pop(i))
+        self.cards = updated_cards
         return thrown_cards
 
     def display_cards(self, visible_cards: List[int]=[]):
@@ -192,44 +281,20 @@ class PlayerI:
         styled = ui.wrap_str_in_stars(card.format)
         print(styled + Colors.ENDC)
 
-    def display_digit_error(self):
-        digit_valid = [*range(1, len(self.cards) + 1)]
-        print('Invalid command, valid commands: {}'.format(digit_valid))
+    def _trigger_queen_effect(self, card_index: int):
+        self.cards[card_index].discover(True)
 
-    def _check_answer(self, answer):
-        if not (self._check_digit(answer) or self._check_command(answer)):
-            digit_valid = [*range(1, len(self.cards) + 1)]
-            command_valid = answer == Game.QuitKey
-            print('Invalid command, valid commands: {}'.format([*digit_valid, command_valid]))
-            return False
-        return True
+    def _trigger_jack_effect(
+        self, 
+        my_card_index: int, 
+        other_card_index: int,
+        other_cards: List[Card]
+        ):
+        self.cards[my_card_index].discover(False)
+        other_cards[other_card_index].discover(False)
+        self.cards[my_card_index], other_cards[other_card_index] = \
+            other_cards[other_card_index], self.cards[my_card_index]
 
-    def _check_digit(self, answer):
-        if not answer.isdigit():
-            return False
-        answer = int(answer)
-        return answer <= len(self.cards) and answer >= 1
-
-    
-    def _check_command(self, answer):
-        if not isinstance(answer, str):
-            return False
-        return answer == Game.QuitKey
-
-    def input_loop(self, msg) -> int:
-        """ loop asking the user to enter an answer: 
-        * digit=index of chosen card).
-        * Game.QuitKey=do not use the action.
-        returns -1 if Game.QuitKey or digit.
-        """
-        self.display_cards()
-        answer = input(msg)
-        while not self._check_answer(answer):
-            self.display_digit_error()
-            answer = input(msg)
-        if answer == Game.QuitKey:
-            return -1
-        return int(answer) - 1
 
 class Player(PlayerI):
     def __init__(self, game):
@@ -237,41 +302,47 @@ class Player(PlayerI):
 
     def play(self, deck_card):
         self._display_deck_card(deck_card)
-        answer = input('Your turn: ')
-        if not self._check_answer(answer):
-            self.play(deck_card)
-        return self._handle_answer(answer, deck_card)
+        inp = PlayerInput()
+        inp.input_loop(self.nb_cards, msg='Your turn: ', accept_indexes=True, accept_commands=True)
+        self.is_kobo = inp.is_kobo
+        return self._handle_input(inp, deck_card)
 
-    def _handle_answer(self, answer, deck_card):
-        if answer.isdigit():
-            answer = int(answer) - 1
-            thrown_cards = self._substitute_card(answer, deck_card)
+    def _handle_input(self, inp: PlayerInput, deck_card: Card):
+        if inp.is_index:
+            thrown_cards = self._substitute_card(inp.value, deck_card)
             self._apply_card_effects(thrown_cards)
             return thrown_cards
-        else:
-            thrown_cards = self._do_not_substitute_card(deck_card)
-            self._apply_card_effects(thrown_cards)
-            return thrown_cards
+        elif inp.is_command:
+            if inp.is_quit_key:
+                thrown_cards = self._do_not_substitute_card(deck_card)
+                self._apply_card_effects(thrown_cards)
+                return thrown_cards
 
     def _apply_card_effects(self, thrown_cards):
         self.game.set_should_display_cards(True)
         for card in thrown_cards:
             if card.rank == Rank.JACK:
-                my_card = self.input_loop('Which card do you wanna switch? ')
-                if my_card == -1:
+                self.display_cards()
+                inp = PlayerInput()
+                inp.input_loop(self.nb_cards, msg='Which card do you wanna switch? ', accept_indexes=True)
+                my_card = inp.value
+                if inp.is_quit_key:
                     break
-                other_card = self.game.ai_player.input_loop('Which card do you wanna peek? ')
-                
-                self.cards[my_card].discover(False)
-                self.game.ai_player.cards[other_card].discover(False)
-                self.cards[my_card], self.game.ai_player.cards[other_card] = \
-                    self.game.ai_player.cards[other_card], self.cards[my_card]
+
+                self.game.ai_player.display_cards()
+                inp = PlayerInput()
+                inp.input_loop(self.game.ai_player.nb_cards, msg='Which card do you wanna peek? ', accept_indexes=True)
+                other_card = inp.value
+                super()._trigger_jack_effect(my_card, other_card, self.game.ai_player.cards)
 
             if card.rank == Rank.QUEEN:
-                hidden_card = self.input_loop('Which card do you wanna see? ')
-                if hidden_card == -1:
+                inp = PlayerInput()
+                inp.input_loop(self.game.ai_player.nb_cards, msg='Which card do you wanna see? ', accept_indexes=True)
+                hidden_card_index = inp.value
+                if inp.is_quit_key:
                     break
-                self.display_cards(visible_cards=[hidden_card])
+                super()._trigger_queen_effect(hidden_card_index)
+                self.display_cards(visible_cards=[hidden_card_index])
                 self.game.set_should_display_cards(False)
 
     def _display_opponent_cards(self):
@@ -287,11 +358,150 @@ class AIPlayer(PlayerI):
         super().__init__(game)
 
     def play(self, deck_card):
-        # return []
-        return self._substitute_card(0, deck_card)
+        def get_worst_card_index(cards: List[Card], rank_to_ignore: Rank=None) -> int:
+            """ returns the index of the card with the worst value.
+            Does not take into account the duplicates.
+            """
+            worst = cards[0], worst_idx = 0
+            for i in range(1, len(cards)):
+                c = cards[i]
+                if c.rank.value > worst and c.rank != rank_to_ignore:
+                    worst = c.rank, worst_idx = i
+            return worst_idx
+
+        def get_worst_combinaison_of_cards_index(cards: List[Card]) -> int:
+            """ returns the index of the card which when played
+            will lose the most points. Takes into account the duplicate
+            cards which will lose more points.
+            """
+            card_values = {}
+            for i in range(len(cards)):
+                c = cards[i]
+                card_values[c.rank] += c.rank
+            max_combinaison_rank = max(card_values.items(), key=operator.itemgetter(1))[0]
+            first_idx = next(c for c in cards if c.rank == max_combinaison_rank)
+            return first_idx
+
+        def get_first_card_index(cards: List[Card], rank: Rank) -> int:
+            for i in range(len(cards)):
+                if cards[i].rank == rank:
+                    return i
+            return -1
+
+        def get_random_card_index(
+            cards: List[Card], 
+            do_not_choose_indexes: List[int]=[]
+            ) -> int:
+            """ returns a pseudo random index from list of cards;
+            some `do_not_choose_indexes` can be provide to avoid 
+            to peek same card multiple times.
+            """
+            if len(cards) == 0:
+                return 0
+            for i in range(len(cards)):
+                if i not in do_not_choose_indexes:
+                    return i
+            return random.randint(0, len(cards))
+
+        def get_hidden_card_index(cards: List[Card]) -> int:
+            """ returns the first index of a hidden card in `cards` or -1.
+            """
+            hidden_cards = [i for i in range(len(cards)) if not cards[i].is_discovered()]
+            return next(hidden_cards) if len(hidden_cards) == 1 else -1
+
+        def apply_card_effects(thrown_cards: List[Card]):
+            def apply_jack_effect(cards: List[Card]):
+                """ apply effect on all jack cards.
+                """
+                already_peeked_indexes = []
+                for c in cards:
+                    if c.rank == Rank.JACK:
+                        random_index = get_random_card_index(player.cards, already_peeked_indexes)
+                        already_peeked_indexes.append(random_index)
+                        super()._trigger_jack_effect(worst_card_idx, random_index, player.cards)
+                        worst_card_idx = get_worst_combinaison_of_cards_index(cards)
+
+            def apply_queen_effect(cards: List[Card]):
+                """ apply effect on all queen cards.
+                """
+                for c in cards:
+                    if c.rank == Rank.QUEEN:
+                        for card in cards:
+                            if not card.is_discovered():
+                                super()._trigger_queen_effect(card)
+                                break
+
+            apply_jack_effect(thrown_cards)
+            apply_queen_effect(thrown_cards)
+            
+
+        player = self.game.player
+        cards = [c for c in self.cards if c.is_discovered()]
+        hidden_card_index = get_hidden_card_index(self.cards)
+
+        # handle kobo
+        if player.is_kobo:
+            if deck_card.rank == Rank.JACK: # deck card is jack
+                thrown_jack_cards = self._do_not_substitute_card(deck_card)
+                apply_card_effects(thrown_jack_cards)
+                return thrown_jack_cards
+            else:
+                jack_index = get_first_card_index(cards, Rank.JACK)
+                if jack_index == -1: # found a jack in cards
+                    thrown_jack_cards = self._substitute_card(jack_index, deck_card)
+                    apply_card_effects(thrown_jack_cards)
+                    return thrown_jack_cards
+        
+        # handle queen
+        if deck_card.rank == Rank.QUEEN:
+            thrown_queen_cards = self._do_not_substitute_card(deck_card)
+            apply_card_effects(thrown_queen_cards)
+            return thrown_queen_cards
+        else:
+            queen_index = get_first_card_index(cards, Rank.QUEEN)
+            if queen_index != -1: # found a queen
+                if hidden_card_index != -1: # found 1 hidden card
+                    thrown_queen_cards = self._substitute_card(queen_index, deck_card)
+                    apply_card_effects(thrown_queen_cards)
+                    return thrown_queen_cards
+
+        # handle hidden cards discover
+        if hidden_card_index != -1: # found a hidden card
+            thrown_cards = self._substitute_card(hidden_card_index, deck_card)
+            apply_card_effects(thrown_cards)
+            return thrown_cards
+        
+        # default turn
+        worst_card_index = get_worst_card_index(cards)
+        thrown_cards = self._substitute_card(worst_card_index, deck_card)
+        return thrown_cards
 
     def _apply_card_effects(self, thrown_cards):
-        pass
+        self.game.set_should_display_cards(True)
+        for card in thrown_cards:
+            if card.rank == Rank.JACK:
+                self.display_cards()
+                inp = PlayerInput()
+                inp.input_loop(self.nb_cards, msg='Which card do you wanna switch? ', accept_indexes=True)
+                my_card = inp.value
+                if inp.is_quit_key:
+                    break
+
+                self.game.ai_player.display_cards()
+                inp = PlayerInput()
+                inp.input_loop(self.game.ai_player.nb_cards, msg='Which card do you wanna peek? ', accept_indexes=True)
+                other_card = inp.value
+                super()._trigger_jack_effect(my_card, other_card, self.game.ai_player.cards)
+
+            if card.rank == Rank.QUEEN:
+                inp = PlayerInput()
+                inp.input_loop(self.game.ai_player.nb_cards, msg='Which card do you wanna see? ', accept_indexes=True)
+                hidden_card_index = inp.value
+                if inp.is_quit_key:
+                    break
+                super()._trigger_queen_effect(hidden_card_index)
+                self.display_cards(visible_cards=[hidden_card_index])
+                self.game.set_should_display_cards(False)
 
     def display_cards(self, visible_cards: List[int]=[]):
         print(Colors.BOLD)
