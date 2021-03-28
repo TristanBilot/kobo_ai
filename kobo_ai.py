34857b530968e20d66b7f581eee4534e91fbbe90
Tristan Bilot <tristan_bilot@carrefour.com>
2021-03-28 21:14:50 +0200

    fix(game): fix some game issues

--- kobo_ai.py
+++ kobo_ai.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import ui_utils as ui
 from enum import Enum
 from dataclasses import dataclass
@@ -161,18 +162,23 @@ class Game:
         while True:
             deck_card = self.pop_card()
             if player_turn:
-                if self._should_display_cards:
-                    player.display_cards(visible_cards=self._displayed_cards)
+                # if self._should_display_cards:
+                #     player.display_cards(visible_cards=self._displayed_cards)
+                # player._display_deck_card(deck_card)
                 thrown_cards = player.play(deck_card)
                 self._displayed_cards = []
             else:
                 ai.display_cards()
+                ai._display_deck_card(deck_card)
                 thrown_cards = ai.play(deck_card)
+            self._throw_duplicate_cards(thrown_cards, player_turn)
             self.thrown_deck += thrown_cards
-            print(('PLAYER' if player_turn else 'AI') + ' PLAYS ' + str(thrown_cards))
+            print(('PLAYER' if player_turn else 'AI') + ' PLAYS')
             visible_cards = [c for c in ai.cards if c.is_discovered()]
             if not player_turn:
                 print(f'Visible cards: {visible_cards}')
+                print(f'Cards: {ai.cards}')
+                print(f'Kobo: {ai.is_kobo}')
 
             player_turn = not player_turn
             if self._check_victory(player, ai):
@@ -200,6 +206,7 @@ class Game:
                 tmp.append(self.pop_card())
             cards.append(tmp.copy())
         self.ai_player.set_cards(cards[0])
+        # self.ai_player.set_cards([Card(Rank.ACE, Suit.CLUB), Card(Rank.NINE, Suit.CLUB), Card(Rank.FOUR, Suit.CLUB), Card(Rank.NINE, Suit.CLUB)])
         self.player.set_cards(cards[1])
 
     def _check_victory(self, player, ai_player):
@@ -218,6 +225,19 @@ class Game:
 
     def set_should_display_cards(self, should: bool):
         self._should_display_cards = should
+    
+    def nb_occurences_in_deck(self, rank: Rank):
+        return len([c for c in self.deck if c.rank == rank])
+
+    def _throw_duplicate_cards(self, thrown_cards: List[Card], player_turn: bool):
+        thrown_ranks = list(map(lambda x: x.rank, thrown_cards))
+        other_player = self.player if not player_turn else self.ai_player
+        for c in other_player.cards:
+            if c.rank in thrown_ranks:
+                print(f'{c} is a duplicate card!')
+                other_player.cards.remove(c)
+                # IMPORTANT  !!!  handle the effects of cards (queen or J if duplicate) use inheritance for methods
+
 
 class PlayerI:
     def __init__(self, game):
@@ -233,6 +253,10 @@ class PlayerI:
     @property
     def nb_cards(self):
         return len(self.cards)
+
+    @property
+    def _hidden_cards(self):
+        return [i for i in range(self.nb_cards) if not self.cards[i].is_discovered()]
 
     def win(self):
         self.victories += 1
@@ -299,7 +323,6 @@ class Player(PlayerI):
         super().__init__(game)
 
     def play(self, deck_card):
-        self._display_deck_card(deck_card)
         inp = PlayerInput()
         inp.input_loop(self.nb_cards, msg='Your turn: ', accept_indexes=True, accept_commands=True)
         self.is_kobo = inp.is_kobo
@@ -356,13 +379,21 @@ class AIPlayer(PlayerI):
         super().__init__(game)
 
     def play(self, deck_card):
-        def get_worst_card_index(cards: List[Card], rank_to_ignore: Rank=None) -> int:
+        def get_best_hit_index(cards: List[Card], rank_to_ignore: Rank=None) -> int:
             """ returns the index of the card with the worst value.
             Does not take into account the duplicates.
             """
             worst, worst_idx = cards[0].rank.value, 0
             for i in range(1, len(cards)):
                 c = cards[i]
+                # if we have a queen and we also have 1 or more hidden cards
+                if c.rank == Rank.QUEEN and len(self._hidden_cards) >= 1:
+                    return i
+                occurences = self.game.nb_occurences_in_deck(c.rank)
+                # if only 1 or 2 card are remaining in the deck
+                if occurences in [1, 2] and not is_a_great_card(c):
+                    return i
+                # get rank with highest value
                 if c.rank.value > worst and c.rank != rank_to_ignore:
                     worst, worst_idx = c.rank.value, i
             return worst_idx
@@ -372,12 +403,14 @@ class AIPlayer(PlayerI):
             will lose the most points. Takes into account the duplicate
             cards which will lose more points.
             """
-            card_values = {}
+            card_values = defaultdict(int)
             for i in range(len(cards)):
                 c = cards[i]
-                card_values[c.rank] += c.rank
+                card_values[c.rank.value] += c.rank.value
             max_combinaison_rank = max(card_values.items(), key=operator.itemgetter(1))[0]
-            first_idx = next(c for c in cards if c.rank == max_combinaison_rank)
+            print(card_values.items())
+            print(max_combinaison_rank)
+            first_idx = next(i for i in range(len(cards)) if cards[i].rank.value == max_combinaison_rank)
             return first_idx
 
         def get_first_card_index(cards: List[Card], rank: Rank) -> int:
@@ -404,36 +437,71 @@ class AIPlayer(PlayerI):
         def get_hidden_card_index(cards: List[Card]) -> int:
             """ returns the first index of a hidden card in `cards` or -1.
             """
-            hidden_cards = [i for i in range(len(cards)) if not cards[i].is_discovered()]
-            return next(iter(hidden_cards)) if len(hidden_cards) > 0 else -1
+            return next(iter(self._hidden_cards)) if len(self._hidden_cards) > 0 else -1
 
-        def apply_card_effects(thrown_cards: List[Card]):
-            def apply_jack_effect(cards: List[Card]):
+        def apply_card_effects(
+            thrown_cards: List[Card], 
+            cards: List[Card],
+            other_player_cards: List[Card],
+            deck_card: Card):
+            def apply_jack_effect(thrown_cards: List[Card], cards: List[Card], other_player_cards: List[Card]):
                 """ apply effect on all jack cards.
                 """
                 already_peeked_indexes = []
-                for c in cards:
-                    if c.rank == Rank.JACK:
-                        random_index = get_random_card_index(player.cards, already_peeked_indexes)
+                for c in thrown_cards:
+                    if c.rank == Rank.JACK and self.game.player.is_kobo: # only use the jack when the other is jack
+                        random_index = get_random_card_index(other_player_cards, already_peeked_indexes)
                         already_peeked_indexes.append(random_index)
-                        super()._trigger_jack_effect(worst_card_idx, random_index, player.cards)
                         worst_card_idx = get_worst_combinaison_of_cards_index(cards)
+                        self._trigger_jack_effect(worst_card_idx, random_index, thrown_cards)
 
-            def apply_queen_effect(cards: List[Card]):
+            def apply_queen_effect(thrown_cards: List[Card], cards: List[Card], deck_card: Card):
                 """ apply effect on all queen cards.
                 """
-                for c in cards:
+                cards = cards.copy() + [deck_card]
+                for c in thrown_cards:
                     if c.rank == Rank.QUEEN:
-                        for card in cards:
+                        for i in range(len(cards)):
+                            card = cards[i]
                             if not card.is_discovered():
-                                super()._trigger_queen_effect(card)
+                                print(f'DISCOVER {card}')
+                                self._trigger_queen_effect(i)
                                 break
 
-            apply_jack_effect(thrown_cards)
-            apply_queen_effect(thrown_cards)
-            
+            apply_jack_effect(thrown_cards, cards, other_player_cards)
+            apply_queen_effect(thrown_cards, cards, deck_card)
 
-        self._display_deck_card(deck_card)
+        def is_a_great_card(card: Card) -> bool:
+            r = card.rank
+            return r == Rank.TEN or r == Rank.ACE or r == Rank.TWO
+
+        def check_kobo(cards: List[Card]) -> bool:
+            for c in cards:
+                if not is_a_great_card(c):
+                    return False
+            return True
+            
+        def throw_deck_card(deck_card: Card):
+            """ plays the peeked deck card.
+            Applies substitutions and effects on cards.
+            """
+            thrown_cards = self._do_not_substitute_card(deck_card)
+            apply_card_effects(thrown_cards, self.cards, self.game.ai_player.cards, deck_card)
+            if check_kobo(self.cards):
+                self.is_kobo = True
+            return thrown_cards
+
+        def throw_player_card(card_index: int, deck_card: Card):
+            """ plays a card within the cards of the player.
+            Applies substitutions and effects on cards.
+            """
+            thrown_cards = self._substitute_card(card_index, deck_card)
+            apply_card_effects(thrown_cards, self.cards, self.game.ai_player.cards, deck_card)
+            if check_kobo(self.cards):
+                self.is_kobo = True
+            return thrown_cards
+
+
         player = self.game.player
         cards = [c for c in self.cards if c.is_discovered()]
         hidden_card_index = get_hidden_card_index(self.cards)
@@ -441,42 +509,57 @@ class AIPlayer(PlayerI):
         # handle kobo
         if player.is_kobo:
             if deck_card.rank == Rank.JACK: # deck card is jack
-                thrown_jack_cards = self._do_not_substitute_card(deck_card)
-                apply_card_effects(thrown_jack_cards)
-                return thrown_jack_cards
+                return throw_deck_card(deck_card)
             else:
                 jack_index = get_first_card_index(cards, Rank.JACK)
                 if jack_index == -1: # found a jack in cards
-                    thrown_jack_cards = self._substitute_card(jack_index, deck_card)
-                    apply_card_effects(thrown_jack_cards)
-                    return thrown_jack_cards
+                    return throw_player_card(jack_index, deck_card)
         
         # handle queen
         if deck_card.rank == Rank.QUEEN:
-            thrown_queen_cards = self._do_not_substitute_card(deck_card)
-            apply_card_effects(thrown_queen_cards)
-            return thrown_queen_cards
+            print('QUEEN DECK')
+            return throw_deck_card(deck_card)
         else:
             queen_index = get_first_card_index(cards, Rank.QUEEN)
             if queen_index != -1: # found a queen
                 if hidden_card_index != -1: # found 1 hidden card
-                    thrown_queen_cards = self._substitute_card(queen_index, deck_card)
-                    apply_card_effects(thrown_queen_cards)
-                    return thrown_queen_cards
+                    print('QUEEN CARDS')
+                    return throw_player_card(queen_index, deck_card)
 
-        print('sdofnsdonsqldnso')
         # handle hidden cards discover
         if hidden_card_index != -1: # found a hidden card
-            print('hidden index')
-            print(hidden_card_index)
-            thrown_cards = self._substitute_card(hidden_card_index, deck_card)
-            apply_card_effects(thrown_cards)
-            return thrown_cards
+            print('HIDDEN')
+            return throw_player_card(hidden_card_index, deck_card)
         
+        # handle every cards are great
+        cards_with_deck_card = cards + [deck_card]
+        best_hit_index = get_best_hit_index(cards_with_deck_card)
+        best_hit_card = cards_with_deck_card[best_hit_index]
+        if is_a_great_card(best_hit_card): # all cards are great
+            self.is_kobo = True
+            best_hit_index = get_worst_combinaison_of_cards_index(cards_with_deck_card)
+            if best_hit_index == len(cards_with_deck_card) - 1: # best hit is the deck card
+                print('KOBO DECK')
+                return throw_deck_card(deck_card)
+            else:
+                print('KOBO CARDS')
+                return throw_player_card(best_hit_index, deck_card)
+
+        # handle deck card is a card we already have
+        deck_card_index = get_first_card_index(cards, deck_card.rank)
+        if deck_card_index != -1 and not is_a_great_card(deck_card):
+            print('ALREADY HAVE THIS CARD')
+            return throw_deck_card(deck_card)
+
         # default turn
-        worst_card_index = get_worst_card_index(cards)
-        thrown_cards = self._substitute_card(worst_card_index, deck_card)
-        return thrown_cards
+        best_hit_index = get_best_hit_index(cards_with_deck_card)
+        if best_hit_index == len(cards_with_deck_card) - 1: # best hit is the deck card
+            print('CLASSIC DECK')
+            return throw_deck_card(deck_card)
+        else:
+            print('CLASSIC CARDS')
+            return throw_player_card(best_hit_index, deck_card)
+        
 
     def display_cards(self, visible_cards: List[int]=[]):
         print(Colors.BOLD)
